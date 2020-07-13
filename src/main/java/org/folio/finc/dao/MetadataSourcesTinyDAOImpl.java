124664fc34e5428edbb5cff73d2d936c02eb5b74
Richard Redweik <redweik@ub.uni-leipzig.de>
2020-07-13 12:37:07 +0200

    Sort tiny metadata sources alphabetically

--- src/main/java/org/folio/finc/dao/MetadataSourcesTinyDAOImpl.java
+++ src/main/java/org/folio/finc/dao/MetadataSourcesTinyDAOImpl.java
@@ -4,6 +4,7 @@ import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.folio.rest.jaxrs.model.TinyMetadataSource;
 import org.folio.rest.jaxrs.model.TinyMetadataSources;
 import org.folio.rest.persist.Criteria.Criterion;
@@ -18,20 +19,24 @@ public class MetadataSourcesTinyDAOImpl implements MetadataSourcesTinyDAO {
   public Future<TinyMetadataSources> getAll(Context vertxContext) {
 
     Promise<TinyMetadataSources> result = Promise.promise();
-
     String tenantId = Constants.MODULE_TENANT;
+
     PostgresClient.getInstance(vertxContext.owner(), tenantId)
         .get(
             TABLE_NAME,
             TinyMetadataSource.class,
             new Criterion(),
             false,
-            false,
             reply -> {
               if (reply.succeeded()) {
                 TinyMetadataSources tinySourcesCollection = new TinyMetadataSources();
                 List<TinyMetadataSource> sources = reply.result().getResults();
-                tinySourcesCollection.setTinyMetadataSources(sources);
+
+                // as we cannot use CQL here (we are querying from a custom view), we need to sort by hand
+                List<TinyMetadataSource> sourcesSorted = sources.stream()
+                    .sorted((o1, o2) -> o1.getLabel().compareToIgnoreCase(o2.getLabel()))
+                    .collect(Collectors.toList());
+                tinySourcesCollection.setTinyMetadataSources(sourcesSorted);
                 tinySourcesCollection.setTotalRecords(
                     reply.result().getResultInfo().getTotalRecords());
                 result.complete(tinySourcesCollection);
