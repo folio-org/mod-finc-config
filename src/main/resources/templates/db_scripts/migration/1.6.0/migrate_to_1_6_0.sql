367964e0ed82849b44b56978f8320af086967039
Richard Redweik <redweik@ub.uni-leipzig.de>
2020-08-03 11:06:43 +0200

    Remove facetLabel in DB migrate script.

--- src/main/resources/templates/db_scripts/migration/1.6.0/migrate_to_1_6_0.sql
+++ src/main/resources/templates/db_scripts/migration/1.6.0/migrate_to_1_6_0.sql
@@ -8,6 +8,10 @@ UPDATE  ${myuniversity}_${mymodule}.metadata_collections
 SET     jsonb = jsonb || '{"collectionId": "n/a"}'::jsonb
 WHERE   jsonb->'collectionId' IS NULL;
 
+UPDATE  ${myuniversity}_${mymodule}.metadata_collections
+SET     jsonb = jsonb - 'facetLabel'
+WHERE   jsonb->'facetLabel' IS NOT NULL;
+
 UPDATE  ${myuniversity}_${mymodule}.metadata_sources
 SET     jsonb = jsonb - 'contracts'
 WHERE   jsonb->'contracts' IS NOT NULL;
