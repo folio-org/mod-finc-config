3ade65d3b48f3dca7b4a70dba8b380c353957ada
Richard Redweik <redweik@ub.uni-leipzig.de>
2020-08-27 13:59:22 +0200

    Rename status 'wish' to 'request'

--- src/main/resources/templates/db_scripts/migration/3.0.0/migrate_to_3_0_0.sql
+++ src/main/resources/templates/db_scripts/migration/3.0.0/migrate_to_3_0_0.sql
@@ -5,4 +5,9 @@ UPDATE  ${myuniversity}_${mymodule}.metadata_sources
 SET     jsonb = jsonb - 'contacts'
 WHERE   jsonb->'contacts' IS NOT NULL;
 
+-- Update status wish to request
+UPDATE  ${myuniversity}_${mymodule}.metadata_sources
+SET     jsonb = jsonb_set(jsonb, '{status}', ('"request"')::jsonb)
+WHERE 	metadata_sources.jsonb->>'status' = 'wish';
+
 END TRANSACTION;
