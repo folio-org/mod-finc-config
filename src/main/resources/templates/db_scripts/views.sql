84cbe929fc432c2fc1410f4abcefcc04ea458bd7
Richard Redweik <rchr@users.noreply.github.com>
2020-09-01 11:07:14 +0200

    Select distinct on name when getting contacts (#104)

--- src/main/resources/templates/db_scripts/views.sql
+++ src/main/resources/templates/db_scripts/views.sql
@@ -2,6 +2,6 @@ CREATE OR REPLACE VIEW metadata_sources_tiny AS SELECT id AS id, jsonb_build_obj
 
 CREATE OR REPLACE VIEW filters_wo_isil AS SELECT id AS id, jsonb - 'isil' AS jsonb FROM filters;
 
-CREATE OR REPLACE VIEW metadata_sources_contacts AS SELECT *
-                                                    FROM (SELECT DISTINCT(jsonb_array_elements(jsonb->'contacts')) AS jsonb from metadata_sources) AS jsonb
-                                                    ORDER BY jsonb->>'name';
+CREATE OR REPLACE VIEW metadata_sources_contacts AS SELECT DISTINCT ON (c.jsonb->>'name') c.jsonb
+                                                    FROM (SELECT jsonb_array_elements(jsonb->'contacts') AS jsonb from finc_mod_finc_config.metadata_sources) AS c
+                                                    ORDER BY c.jsonb->>'name';
