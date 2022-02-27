8101de2439bffaf190d57ff3d5d060eb4569f113
alb3rtino <alb3rtino@users.noreply.github.com>
2022-02-27 14:08:26 +0100

    Honor proxy settings for EZBServiceImpl

--- src/main/java/org/folio/finc/periodic/ezb/EZBServiceImpl.java
+++ src/main/java/org/folio/finc/periodic/ezb/EZBServiceImpl.java
@@ -5,9 +5,15 @@ import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.net.ProxyOptions;
 import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
 
 public class EZBServiceImpl implements EZBService {
 
@@ -21,6 +27,22 @@ public class EZBServiceImpl implements EZBService {
 
   public EZBServiceImpl() {}
 
+  private Optional<ProxyOptions> getProxyOptions(String url) {
+    try {
+      return ProxySelector.getDefault().select(new URI(url)).stream()
+          .filter(p -> p.address() != null)
+          .findFirst()
+          .map(
+              p -> {
+                InetSocketAddress addr = (InetSocketAddress) p.address();
+                return new ProxyOptions().setHost(addr.getHostName()).setPort(addr.getPort());
+              });
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+      return Optional.empty();
+    }
+  }
+
   @Override
   public Future<String> fetchEZBFile(String user, String password, String libId, Vertx vertx) {
     Promise<String> result = Promise.promise();
@@ -28,6 +50,7 @@ public class EZBServiceImpl implements EZBService {
     String formattedUrl = String.format(url, libId);
     HttpRequest<Buffer> get =
         client.requestAbs(HttpMethod.GET, formattedUrl).basicAuthentication(user, password);
+    getProxyOptions(formattedUrl).ifPresent(get::proxy);
     get.send(
         ar -> {
           if (ar.succeeded()) {
--- src/test/java/org/folio/finc/periodic/ezb/EZBServiceImplTest.java
+++ src/test/java/org/folio/finc/periodic/ezb/EZBServiceImplTest.java
@@ -1,6 +1,7 @@
 package org.folio.finc.periodic.ezb;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
@@ -12,6 +13,15 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.vertx.core.Vertx;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+import java.net.ProxySelector;
+import java.net.SocketAddress;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -25,11 +35,14 @@ public class EZBServiceImplTest {
   private static final String CONTENT = "content";
   private static final String ISIL = "isil1";
   private static EZBServiceImpl ezbService;
+  private static final ProxySelector defaultProxySelector = ProxySelector.getDefault();
 
   @Rule public WireMockRule wmRule = new WireMockRule(new WireMockConfiguration().dynamicPort());
+  @Rule public WireMockRule proxyRule = new WireMockRule(new WireMockConfiguration().dynamicPort());
 
   @Before
   public void setUp() {
+    ProxySelector.setDefault(defaultProxySelector);
     ezbService = new EZBServiceImpl(wmRule.url("%s"));
   }
 
@@ -74,5 +87,39 @@ public class EZBServiceImplTest {
         .onComplete(
             context.asyncAssertFailure(
                 t -> assertThat(t).hasMessageContaining("Failed to fetch ezb file.")));
+  }
+
+  @Test
+  public void testFetchEZBFileWithProxy(TestContext context) {
+    ProxySelector.setDefault(
+        new ProxySelector() {
+          @Override
+          public List<Proxy> select(URI uri) {
+            return Collections.singletonList(
+                new Proxy(Type.HTTP, new InetSocketAddress("localhost", proxyRule.port())));
+          }
+
+          @Override
+          public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {}
+        });
+
+    wmRule.stubFor(
+        get(urlEqualTo("/" + ISIL)).willReturn(aResponse().withStatus(200).withBody(CONTENT)));
+    proxyRule.stubFor(get(anyUrl()).willReturn(aResponse().proxiedFrom(wmRule.baseUrl())));
+    ezbService
+        .fetchEZBFile(USER, PASS, ISIL, Vertx.vertx())
+        .onComplete(
+            context.asyncAssertSuccess(
+                s -> {
+                  proxyRule.verify(
+                      1,
+                      getRequestedFor(urlEqualTo("/" + ISIL))
+                          .withBasicAuth(new BasicCredentials(USER, PASS)));
+                  wmRule.verify(
+                      1,
+                      getRequestedFor(urlEqualTo("/" + ISIL))
+                          .withBasicAuth(new BasicCredentials(USER, PASS)));
+                  assertThat(s).isEqualTo(CONTENT);
+                }));
   }
 }
