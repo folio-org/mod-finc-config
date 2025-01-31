074cf5a17db8e1bfe414ba6e71f9996c2a8801e5
alb3rtino <alb3rtino@users.noreply.github.com>
2025-01-31 12:57:46 +0100

    [UIFC-406] Increase maximum allowed string length for RMBs ObjectMapper (#161)

--- src/main/java/org/folio/rest/FincConfigLauncher.java
+++ src/main/java/org/folio/rest/FincConfigLauncher.java
@@ -5,8 +5,11 @@ import com.fasterxml.jackson.core.StreamReadConstraints;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.json.jackson.DatabindCodec;
+import org.folio.dbschema.ObjectMapperTool;
 
 public class FincConfigLauncher extends RestLauncher {
+
+  static final int MAX_STRING_LEN = 50 * 1024 * 1024; // 50MB
 
   public static void main(String[] args) {
     new FincConfigLauncher().dispatch(args);
@@ -19,11 +22,16 @@ public class FincConfigLauncher extends RestLauncher {
     ObjectMapper objectMapper = DatabindCodec.mapper();
     JsonFactory factory = objectMapper.getFactory();
 
-    // Modify only the max string length constraint
+    // Modify the max string length constraint for Vertx ObjectMapper
     factory.setStreamReadConstraints(
-        StreamReadConstraints.builder()
-            .maxStringLength(50 * 1024 * 1024) // Increase limit to 50 MB
-            .build());
+        StreamReadConstraints.builder().maxStringLength(MAX_STRING_LEN).build());
+
+    // and for the ObjectMapper used by RMB
+    ObjectMapperTool.getMapper()
+        .getFactory()
+        .setStreamReadConstraints(
+            StreamReadConstraints.builder().maxStringLength(MAX_STRING_LEN).build());
+
     System.out.println(
         "Increased max string length to 50 MB for JSON parsing. (FincConfigLauncher)");
   }
