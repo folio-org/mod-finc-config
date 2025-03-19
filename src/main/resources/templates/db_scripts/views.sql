34868229713da65f73e911ed23fb2dd2f5228ce1
alb3rtino <alb3rtino@users.noreply.github.com>
2025-03-19 17:23:15 +0100

    Add `includeFilteredBy` query parameter to endpoints to return metadata collections with filters
    Add database view `metadata_collections_w_filters`
    Refactor API method to use PGUtil

--- src/main/resources/templates/db_scripts/views.sql
+++ src/main/resources/templates/db_scripts/views.sql
@@ -5,3 +5,12 @@ CREATE OR REPLACE VIEW filters_wo_isil AS SELECT id AS id, jsonb - 'isil' AS jso
 CREATE OR REPLACE VIEW metadata_sources_contacts AS SELECT DISTINCT ON (c.jsonb->>'name') c.jsonb
                                                     FROM (SELECT jsonb_array_elements(jsonb->'contacts') AS jsonb from finc_mod_finc_config.metadata_sources) AS c
                                                     ORDER BY c.jsonb->>'name';
+
+CREATE OR REPLACE VIEW metadata_collections_w_filters AS
+  SELECT
+    collections.id as id,
+    jsonb_set(collections.jsonb, '{filteredBy}', coalesce(jsonb_agg(filters.jsonb) FILTER (WHERE filters.id IS NOT NULL), '[]'::jsonb)) as jsonb
+  FROM metadata_collections collections
+  LEFT JOIN filter_to_collections f2c ON f2c.jsonb->'collectionIds' @> to_jsonb(collections.id)
+  LEFT JOIN filters filters ON f2c.id = filters.id
+  GROUP BY collections.id;
